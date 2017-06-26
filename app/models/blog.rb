56a053752265417c7fa55562c3a84d5ed061d8a6
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 15:05:26 +0000

    updated status for enums

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,7 @@
 class Blog < ApplicationRecord
-  class User < ApplicationRecord
-  extend FriendlyId
+ enum status: { draft: 0, published: 1 }
+ extend FriendlyId
   friendly_id :title, use: :slugged
   end
 
-end
+
