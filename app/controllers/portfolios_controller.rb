aa3906be8cba9dcc0f22109244af8610dd6e4cb2
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 17:17:14 +0000

    refactored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update,  :destroy, :show]
   layout 'portfolio'
   
   def index
@@ -15,7 +16,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(portfolio_params)
+    
 
     respond_to do |format|
       if @portfolio_item.save
@@ -27,12 +28,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -43,13 +41,10 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
     # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-
     # Destroy/delete the record
     @portfolio_item.destroy
 
@@ -68,5 +63,8 @@ class PortfoliosController < ApplicationController
                                       technologies_attributes: [:name]
                                      )
   end
-
+  
+  def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
+  end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,6 +6,10 @@ class Blog < ApplicationRecord
   validates_presence_of :title, :body
   
   belongs_to :topic
+  
+  def self.special_blogs
+   all
+  end
 end
 
 
